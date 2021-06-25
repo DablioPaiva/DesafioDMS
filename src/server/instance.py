259764f4a622acb9b3dfe372acea7ae2e2b41d49
Welington Paiva <51125125+DablioPaiva@users.noreply.github.com>
2021-06-24 22:28:17 -0300

    alterando import

--- src/server/instance.py
+++ src/server/instance.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import app
+from flask import API
 
 class Server():
     def __init__(self,):
         self.app = Flask(__name__)
-        self.api = app(self.app,
+        self.api = API(self.app,
             version="1,0",
-            title="API Olimpíadas",
+            title="Desafio DMS Olimpíadas",
             description="API Olimpíadas",
             doc="/docs"
         )
