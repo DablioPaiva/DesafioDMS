93a60c5b2287f29b43082b6634c88b0a1fb1c439
Welington Paiva <51125125+DablioPaiva@users.noreply.github.com>
2021-06-24 22:30:04 -0300

    alterando variável

--- src/controllers/resultados.py
+++ src/controllers/resultados.py
@@ -2,19 +2,18 @@ from src.server.instance import server
 import sqlite3
 from ..tratamento.convert_salvar import ConverteMetros, ConverterTempo
 
-app, api = server.app, server.api
+app, API = server.app, server.api
 
 
-@api.route("/resultados")
+@app.route("/resultados")
 class Resultados():
     def post(self, ):
-        # pegar dados enviados
-        request = api.payload
+        request = app.payload
         id_competicao = request['competicao']
         atleta = request['atleta']
         valor = request['valor']
         unidade = request['unidade']
-        # verificar qual a modalidade
+        
         conn = sqlite3.connect('radar.db')
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM competicoes WHERE id=:id',
@@ -25,30 +24,28 @@ class Resultados():
         modalidade = resultado[2]
         nome_competicao = resultado[1]
 
-        # Tratar dados (cenverter para menor unidade de medida)
         if resultado[3] == 1:
-            if modalidade == "yoga":
+            if modalidade == "100M Rasos":
                 valor = ConverterTempo(valor, unidade)
 
-            if modalidade == "hidratacao":
-                valor = ConverteMetros(valor, unidade)
-        # Salvar na tabela de resultados
             if modalidade != "lancamento de dardos":
                 cursor.execute(
                     'SELECT * FROM resultados WHERE atleta=:atleta AND modalidade=:modalidade', {"atleta": atleta, "modalidade": modalidade})
                 if cursor.fetchall():
                     conn.close
                     return "Atleta já Participou dessa Competição", 400
+                
             else:
                 cursor.execute(
                     'SELECT count(1) FROM resultados WHERE atleta=:atleta AND modalidade=:modalidade', {"atleta": atleta, "modalidade": modalidade})
                 c = cursor.fetchall()[0][0]
                 if c >= 3:
                     conn.close
-                    return "Atleta já teve 3 tentativas nessa competição", 400
+                    return "Atleta já teve 3 tentativas na competição", 400
 
-            cursor.execute('INSERT INTO resultados VALUES (NULL, ?,?,?,?,?)',
+            cursor.execute('INSERT INTO resultados VALUES (NULL)',
                            (id_competicao, nome_competicao, modalidade, atleta, valor))
             conn.commit()
+            
             return 200
         return "Essa Competição já foi finalizada", 400
